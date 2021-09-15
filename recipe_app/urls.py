499c359002bf42ed08fb47e6c54988efe23f5a29
bluairis <87868024+bluairis@users.noreply.github.com>
2021-09-14 22:39:37 -0500

    this is what I have so far

--- recipe_app/urls.py
+++ recipe_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:recipe_id>/', views.recipe_page, name='recipe_page'),
     path('<int:recipe_id>/delete', views.delete_recipe, name='delete_recipe'),
-    path('<int:recipe_id>/<str:ingredient_name>/delete', views.delete_ingredient, name='delete_ingredient'),
-    path('<int:recipe_id>/<str:direction_name>/delete', views.delete_direction, name='delete_direction'),
+    path('<int:recipe_id>/<str:ingredient_name>/delete_ingredient', views.delete_ingredient, name='delete_ingredient'),
+    path('<int:recipe_id>/<str:step>/delete_direction', views.delete_direction, name='delete_direction'),
     path('add', views.add_recipe, name='add_recipe'),
 ]
